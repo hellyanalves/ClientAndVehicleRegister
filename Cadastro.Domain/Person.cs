274e95ca9211c165ca10d0f0a7dbe1ae368bdd0f
hellyanalves <hellyanalves@gmail.com>
2020-02-13 00:49:29 -0300

    scripts, domain and infrastructure basics

--- Cadastro.Domain/Person.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cadastro.Domain
-{
-    public class Person
-    {
-    }
-}
--- Person.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class Person
-{
-	public int Id { get; set; }
-	public string Name { get; set; }
-	public 
-}
